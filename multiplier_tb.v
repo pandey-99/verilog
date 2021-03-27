5d422dcf67caee8eedfc1517c00a848d07e73f91
Prince Kumar Pandey <pandeyprince9999@gmail.com>
2021-03-27 23:04:19 +0530

    Update multiplier_tb.v

--- multiplier_tb.v
+++ multiplier_tb.v
@@ -1,3 +1,6 @@
+//testbench for multiplier using data path and control path
+
+
 `timescale 1ns/1ps
 `include "control_data.v"
 
@@ -9,19 +12,20 @@ wire done;
 mul_data DP(equalZ,loadA,loadB,loadP,clrP,decB,data_in,clk);
 controller CON (loadA,loadB,loadP,clrP,decB,done,clk,equalZ,start);
 initial
-begin
-clk=1'b0;
-#3 start=1'b1;
-#500 $finish;
-end
-always #5 clk=~clk;
-initial begin
-#10 data_in=15;
-#20 data_in=9;
-end
+  begin
+    clk=1'b0;                  // setting the initial value of the clock
+    #3 start=1'b1;             // set the start signal to to 1 to start the operation
+    #100 $finish;              // finish the operation at 100ns
+  end
+always #5 clk=~clk;        // toggling the clock every 5ns
+  initial 
+    begin 
+      #10 data_in=15;           // data in is provided on the bus
+      #20 data_in=9;
+    end
 initial
-begin
-$monitor($time,"%d %b", DP.Y,done);
-$dumpfile("mul.vcd"); $dumpvars (0,mul_test);
-end
+   begin
+     $monitor($time,"%d %b", DP.Y,done);                  // to see the output on the moniter 
+     $dumpfile("mul.vcd"); $dumpvars (0,mul_test);        // variable values dump
+   end
 endmodule
