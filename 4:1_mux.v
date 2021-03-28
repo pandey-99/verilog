3850ff59164f05eaf38bb329f9155f1ca150c870
Prince Kumar Pandey <pandeyprince9999@gmail.com>
2021-03-28 22:57:19 +0530

    Create 4:1_mux.v

---
+++ 4:1_mux.v
@@ -0,0 +1,12 @@
+//4:1 mux
+ 
+ `timescale 1ns/1ps 
+
+  module mux(a,b,c,d,S0,S1,op);  
+
+     input [3:0] a,b,c, b;
+     input  S0,S1;
+     output[3:0] op;
+     assign op= S1? (S0?a:b):(S0?c:d);
+
+  endmodule
