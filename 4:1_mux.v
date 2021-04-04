4a7b3f11ff82f0574930d823e536bc0456b5d1f9
Prince Kumar Pandey <pandeyprince9999@gmail.com>
2021-04-04 23:54:43 +0530

    Update 4:1_mux.v

--- 4:1_mux.v
+++ 4:1_mux.v
@@ -4,8 +4,8 @@
 
   module mux(a,b,c,d,S0,S1,op);  
 
-     input [3:0] a,b,c, b;
-     input  S0,S1;
+   input [3:0] a,b,c, b;                             //4 bit input a,b,c,d
+     input  S0,S1;                                   // S0,S1 is select line
      output[3:0] op;
      assign op= S1? (S0?a:b):(S0?c:d);
 
